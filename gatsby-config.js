0faa91625d8fcf70fe3f4975c8dd0b1dec52688b
Rhys Clay <rhys@theitteam.com.au>
2018-05-21 16:06:32 +1000

    Added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,24 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/assets/images/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
